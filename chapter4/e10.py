c122b49850673355360380d88ca4e9df676e7dc3
bigen1925 <bigen1925@gmail.com>
2020-02-02 16:25:16 +0900

    4-12やった

---
+++ chapter4/e10.py
@@ -0,0 +1,19 @@
+import unittest
+from typing import List, Any
+from copy import copy
+
+from .Graph import BinaryTree, BinaryNode
+
+
+class TreeE10(BinaryTree):
+    def is_subtree_of(self, tree: BinaryTree):
+        pass
+
+
+class Test(unittest.TestCase):
+    def test_it1(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()

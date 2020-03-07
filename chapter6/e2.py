79f4913e650ba81540c3548e4827ad14aeaf4b7c
bigen1925 <bigen1925@gmail.com>
2020-03-07 17:07:01 +0900

    Create e2.py

---
+++ chapter6/e2.py
@@ -0,0 +1,15 @@
+###
+# When
+# 0 < p < 1
+# P1 = p
+# P2 = p^2 (1-p) * 3 + p^3
+#
+# Solve
+# P1 - P2 > 0
+# <=> p - (-2p^3 + 3p^2) > 0
+# <=> 2p^2 - 3p + 1 > 0 (because p > 0)
+# <=> (2p - 1)(p - 1) > 0
+# p < 1/2
+
+# Answer
+# You should choice game 1 when probability of goal is lesser than 1/2.
